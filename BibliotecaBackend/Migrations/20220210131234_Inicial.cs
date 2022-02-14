abfa55e7eb3c506144858f879d77d18d388f2e6d
tetri.neto <tetri.neto@datacoper.com.br>
2022-02-14 15:43:16 -0300

    o commit mais bonito do github

---
+++ BibliotecaBackend/Migrations/20220210131234_Inicial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibliotecaBackend.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Obras",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    editora = table.Column<string>(type: "TEXT", nullable: false),
+                    foto = table.Column<string>(type: "TEXT", nullable: true),
+                    autores = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Obras", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Obras");
+        }
+    }
+}
